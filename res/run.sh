8fe190b5eceff089ab6ce2b2ce33d1bf6c030316
ying.yunlong <ying.yunlong@zte.com.cn>
2018-03-26 14:40:50 +0800

    Modify vfc-vnfres log export directory
    Change-Id: I52feefcdb43495f2f10a39b87eaf03c6428d054a
    Issue-ID: VFC-839
    Signed-off-by: ying.yunlong <ying.yunlong@zte.com.cn>

--- res/run.sh
+++ res/run.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-logDir="/var/log/onap/vfc-gvnfm-vnfres/"
+logDir="/var/log/onap/vfc/gvnfm-vnfres/"
 if [ ! -x  $logDir  ]; then
        mkdir -p $logDir
 fi
