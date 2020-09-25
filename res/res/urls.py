dcb11bc9dd7f3d8bb55a12a3802b903beaf04290
dengyh <dengyuanhong@chinamobile.com>
2020-09-25 16:43:09 +0800

    Fixed duplicate oom registration for local programs
    Issue-ID: VFC-1752
    Change-Id: I084fbe722aea9eb6ae024580b37895143e96de40
    Signed-off-by: dengyh <dengyuanhong@chinamobile.com>

--- res/res/urls.py
+++ res/res/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
 ]
 
 # regist to MSB when startup
-if REG_TO_MSB_WHEN_START:
+if REG_TO_MSB_WHEN_START == "true":
     import json
     from res.pub.utils.restcall import req_by_msb
     req_by_msb(MSB_SVC_URL, "DELETE")
